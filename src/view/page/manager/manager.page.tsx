0a53fd89d3247b09ad94a14ce21c21670a2ac416
aminulislamabid <aminul.i.abid@gmail.com>
2024-05-26 22:49:30 +0600

    #1 'User can't delete the parking lot' fixed

--- src/view/page/manager/manager.page.tsx
+++ src/view/page/manager/manager.page.tsx
@@ -1,11 +1,33 @@
+import { deleteParkingLot } from "@/api/api";
 import ParkingSlot from "@/components/parking-slot";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { toast } from "@/components/ui/use-toast";
 import { ParkingLot, ParkingSlotProps } from "@/constants";
 import useParkingLots from "@/hooks/useParkingLots";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Trash2 } from "lucide-react";
 import CreateParkingLot from "./components/create-parking-lot";
 
 const Manager = () => {
+  const queryClient = useQueryClient();
   const { data: parkingLots, isLoading, isError } = useParkingLots();
+
+  const {
+    mutate: mutateDelete,
+    isPending: isDeletePending,
+    isSuccess: isDeleteSuccess,
+    isError: isDeleteError,
+    error: deleteError,
+    data: deleteData,
+  } = useMutation({
+    mutationFn: deleteParkingLot,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["parking-lots"],
+      });
+    },
+  });
 
   if (isLoading) {
     return (
@@ -20,6 +42,21 @@ const Manager = () => {
       variant: "destructive",
       title: "Error",
       description: "Error occurred showing parking lots try again",
+    });
+  }
+
+  if (isDeleteError) {
+    toast({
+      variant: "destructive",
+      title: "Error",
+      description: deleteError.message,
+    });
+  }
+
+  if (isDeleteSuccess) {
+    toast({
+      title: "Success",
+      description: deleteData.message,
     });
   }
 
@@ -42,10 +79,35 @@ const Manager = () => {
         </div>
       )}
 
-      <div className="grid grid-cols-1 gap-4 mt-10">
+      <div
+        className={`grid grid-cols-1 gap-4 mt-10 ${
+          isDeletePending ? "opacity-50 pointer-events-none" : ""
+        }`}
+      >
         {parkingLots.map((lot: ParkingLot) => (
           <div key={lot.id}>
-            <h2 className="text-lg font-semibold mb-6">{lot.name}</h2>
+            <div className="flex justify-between items-center mb-6">
+              <h2 className="text-lg font-semibold">{lot.name}</h2>
+              <Separator className="w-4/5" />
+              <Button
+                variant="destructive"
+                size="sm"
+                onClick={() => {
+                  if (lot.id) {
+                    mutateDelete(lot.id);
+                  } else {
+                    toast({
+                      variant: "destructive",
+                      title: "Error",
+                      description:
+                        "Delete parking lot failed try again refresh the page",
+                    });
+                  }
+                }}
+              >
+                <Trash2 />
+              </Button>
+            </div>
             <div className="flex justify-center">
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
                 {lot.slots?.map((slot: ParkingSlotProps) => (
